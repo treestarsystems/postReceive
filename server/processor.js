4f4a92ccd714285028d3288112dd110d46d713a0
Michael Bogle <mbogle@mjns.it>
2019-07-09 07:14:40 -0400

    stuff

--- server/processor.js
+++ server/processor.js
@@ -5,6 +5,7 @@ const path = require('path');
 const core = require(path.join(__dirname, 'prmodules/prModules.js'));
 const simpleParser = require('mailparser').simpleParser;
 const axios = require('axios');
+const readline = require('readline');
 
 var dirName = path.join(__dirname, '../message_storage');
 var prMessage = {};
@@ -13,44 +14,31 @@ prMessage.prId = core.genRegular(20);
 prMessage.filename = `message-${prMessage.timestamp}-${prMessage.prId}.eml`;
 prMessage.prFullFilePath = `${dirName}\/${prMessage.filename}`;
 
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-//  output: process.stdout
-});
-
 function readStdIn () {
+	const rl = readline.createInterface({
+		input: process.stdin,
+	//	output: process.stdout
+	});
+
 	var writeStream = fs.createWriteStream(prMessage.prFullFilePath);
 	rl.on('line', (line) => {
-		writeStream.write(line + '\r\n', {encoding:'utf8', mode: "770", flag: 'a'}, () => { writeStream.end(); });
+		/*
+		This was a bit tough to get right. I wanted something that would trigger when things were done writing 
+		ALL LINES to the disk. Found a post that said to use the .end as a callback and that was it.
+		POST: https://stackoverflow.com/a/27770204
+		*/
+		//I need to work on the permissions later.
+		writeStream.write(line + '\r\n', {encoding:'utf8', mode: 0o770, flag: 'w'}, () => { writeStream.end(); });
 	});
 	writeStream.on('finish', () => {
-	axios.post('http://localhost:5000/api/message', prMessage)
-		.then(function (response) {
-	      		console.log(response.data);
-	        })
-	        .catch(function (error) {
-	              console.log(error);
-	        });
+		axios.post('http://localhost:5000/api/message', prMessage)
+			.then(function (response) {
+	      			console.log(response.data);
+	        	})
+	        	.catch(function (error) {
+	              		console.log(error);
+	        	});
 	});
 }
 
 readStdIn ();
-
-//console.log(JSON.stringify(prMessage));
-/*
-    prMessage.to = parsed.headers.get('from').value;
-    prMessage.to = parsed.headers.get('to').value;
-if (parsed.headers.has('cc')) {
-    prMessage.cc = parsed.headers.get('cc').value;
-}
-    prMessage.messagedate = parsed.headers.get('date');
-    prMessage.messageid = parsed.headers.get('messageId');
-    prMessage.inreplyto = parsed.headers.get('inReplyTo');
-    prMessage.replyto = parsed.headers.get('reply-to').value;
-    prMessage.references = parsed.headers.get('references');
-    prMessage.html = parsed.headers.html;
-    prMessage.text = parsed.headers.text;
-    prMessage.textashtml = parsed.headers.get('textAsHtml');
-*/
-
--- server/routes/api/message.js
+++ server/routes/api/message.js
@@ -3,23 +3,87 @@ const router = express.Router();
 const loadCollection = require('../../prmodules/prModules').loadCollection;
 const simpleParser = require('mailparser').simpleParser;
 const bodyParser = require('body-parser');
-
-let now = new Date();
+const fs = require('fs');
 
 // Get Message Data
 router.get('/', async (req, res) => {
-  console.log(`success ${now}`);
   console.log(req);
 //  let parsed = await simpleParser(req);
 //  res.status(200).send('DONE');
 });
 
-router.use(bodyParser.json());
 //Add Single Message
+router.use(bodyParser.json());
 router.post('/', async (req, res) => {
-  console.log(`success ${now}`);
-  console.log(req.body);
-  res.status(200).send('DONE');
+	processMessage (req.body.prFullFilePath);
+	res.status(200).send('DONE');
 });
+
+function processMessage (filePath) {
+	prProcessedrMessage = {};
+	fs.readFile(filePath, async (err, data) => {
+		if (err) throw err;
+		let parsed = await simpleParser(data);
+		prProcessedrMessage.fullMessage = data.toString();
+		if (parsed.headers.has('subject')) {
+			prProcessedrMessage.subject = parsed.headers.get('subject');
+		}
+		if (parsed.headers.has('from')) {
+			prProcessedrMessage.from = parsed.headers.get('from').value[0];
+		}
+		if (parsed.headers.has('to')) {
+			prProcessedrMessage.to = parsed.headers.get('to').value[0];
+		}
+		if (parsed.headers.has('cc')) {
+			prProcessedrMessage.cc = parsed.headers.get('cc').value[0];
+		}
+		if (parsed.date) {
+			//[DATE-ISO,DATE-HUMAN,DATE-EPOCH/MILLISECOND]
+			prProcessedrMessage.timestamps = [parsed.date,Date(parsed.date),new Date(parsed.date).getTime()];
+		}
+		if (parsed.messageId) {
+			prProcessedrMessage.messageid = parsed.messageId;
+		}
+		if (parsed.headers.has('inReplyTo')) {
+			prProcessedrMessage.inreplyto = parsed.inReplyTo;
+		}
+		if (parsed.headers.has('reply-to')) {
+			prProcessedrMessage.replyto = parsed.headers.get('reply-to').value[0];
+		}
+		if (parsed.references) {
+			prProcessedrMessage.references = parsed.references;
+		}
+		prProcessedrMessage.html = parsed.html;
+		prProcessedrMessage.text = parsed.text;
+		prProcessedrMessage.textashtml = parsed.textAsHtml;
+		/* I need the following from the attachments
+			filename
+			contentType
+			contentDisposition
+			checksum
+			size
+			headers
+				content-type
+				content-transfer-encoding
+					encoding types: "BASE64" / "QUOTED-PRINTABLE" / "8BIT" / "7BIT" / "BINARY" / x-token
+				content-disposition
+				content-id
+			content
+			contentId
+			cid
+			related
+		*/
+		prProcessedrMessage.attachments = parsed.attachments;
+//		console.log(prProcessedrMessage);
+		console.log(prProcessedrMessage.html);
+
+//USE for later-console.log(prProcessedrMessage.attachments[0].headers.get('content-disposition'));
+		fs.unlink(filePath, function (err) {
+			if (err) throw err;
+			// if no error, file has been deleted successfully
+			console.log('\nFile deleted!');
+		});
+	});
+}
 
 module.exports = router;
